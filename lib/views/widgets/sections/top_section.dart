0996c3cf66200a95d72548a5c6f58e7a66d851ac
João Oliveira <j.oliveiradg@gmail.com>
2023-03-11 14:25:41 -0300

    [Dia 62] - feat: Criando a Seção do Topo para Tablet

--- lib/views/widgets/sections/top_section.dart
+++ lib/views/widgets/sections/top_section.dart
@@ -61,56 +61,75 @@ class TopSection extends StatelessWidget {
 
       }
       
-      if (maxWidth >= 800) {
-        return AspectRatio(
-          aspectRatio: 3.2,
-          child:  Stack(
+      if(maxWidth >= 800){
+        return SizedBox(
+          height: 300,
+          child: Stack(
             children: [
-            AspectRatio(
-              aspectRatio: 3.2,
-              child:   Image.network('https://www.apexon.com/wp-content/uploads/2022/09/flutter-3-1.png',
+              SizedBox(
+                height: 250,  
+                width: double.infinity,          
+                child:   Image.network('https://www.apexon.com/wp-content/uploads/2022/09/flutter-3-1.png',
         fit: BoxFit.cover,
-        ),
-              
-              ),
-           Positioned(
-            left: 50,
-            top: 50,
-            child: Card(
-             color: Colors.black,
-             elevation: 8,
-             child: Container(
-                width: 500,
-                padding: const EdgeInsets.all(16),
-               child: Column(
-                 children: const [
-                   Text('Aprenda Flutter 3.0 com o melhor do mercado. ',
-                   style: TextStyle(
-                     color: Colors.white,
-                     fontSize: 40,
-                     fontWeight: FontWeight.bold,
-                   ),
-                   ),
-                    SizedBox(height: 8,),
-                   Text('Bora aprender Flutter por apenas R\$22,90! ',
-                   style: TextStyle(
-                     color: Colors.white,
-                     fontSize: 18,
-                     fontWeight: FontWeight.bold,
-                   ),
-                   ),
-                    SizedBox(height: 16,),
-                    CustomSearch(),
-                 ],
-               ),
-             ),
-            ),
-          ),
 
-            ],
-          ),
+                ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 20,
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 8,
+                    child: Container(
+                      width: 300,
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: const [
+                          Text('Aprenda Flutter 3.0 com o melhor do mercado. ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                           SizedBox(height: 8,),
+                          Text('Bora aprender Flutter por apenas R\$22,90! ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                           SizedBox(height: 16,),
+                           CustomSearch(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+            ]
+
+
+          )
+
         );
       }
+
+
+
+
+
+
+
+
+      // if (maxWidth >= 800) {
+      //   return 
+      //         child:   Image.network('https://www.apexon.com/wp-content/uploads/2022/09/flutter-3-1.png',
+      //   fit: BoxFit.cover,
+      //   ),
+             
+
+      
       return Container();
     }));
   }
