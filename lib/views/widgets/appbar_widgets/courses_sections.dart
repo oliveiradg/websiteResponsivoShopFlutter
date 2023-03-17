dba7fd22c46c6c293c87bd0bdba9cff2d217fb17
João Oliveira <j.oliveiradg@gmail.com>
2023-03-16 21:36:53 -0300

    [Dia 67] - feat: finalizando a Grade de Cursos Responsiva

--- lib/views/widgets/appbar_widgets/courses_sections.dart
+++ lib/views/widgets/appbar_widgets/courses_sections.dart
@@ -6,26 +6,22 @@ class CoursesSections extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return Container(
-        color: Colors.red,
-        height: 200,
-        width: constraints.maxWidth,
-        child: GridView.builder(
+    return LayoutBuilder(builder: (_, constraints) {
+      return  GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 300,
-            crossAxisSpacing: 4,
-            mainAxisSpacing: 4,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
           ),
           padding: EdgeInsets.symmetric(
-              vertical: 16, horizontal: constraints.maxWidth >= 1200 ? 0 : 16),
+              vertical: 16, horizontal: constraints.maxWidth >= 800 ? 0 : 16),
           physics: const NeverScrollableScrollPhysics(),
           shrinkWrap: true,
           itemCount: 20,
           itemBuilder: (context, index) {
-            return Container();
+            return Container(color: Colors.red,);
           },
-        ),
+        
       );
     });
 
